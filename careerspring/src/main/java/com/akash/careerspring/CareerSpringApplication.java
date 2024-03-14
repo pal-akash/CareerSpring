af45ca50f0f3362f3924d15c12366e686483b762
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2024-03-14 13:36:12 +0530

    [init] initialized project

---
+++ careerspring/src/main/java/com/akash/careerspring/CareerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.akash.careerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerSpringApplication.class, args);
+	}
+
+}
---
+++ careerspring/src/test/java/com/akash/careerspring/CareerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.careerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
