e5e0640f729686dfdaa1a8ebd9608beb4833f0ae
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2024-03-22 20:28:44 +0530

    create company entity

---
+++ careerspring/src/main/java/com/akash/careerspring/company/Company.java
@@ -0,0 +1,56 @@
+package com.akash.careerspring.company;
+
+import com.akash.careerspring.job.Job;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @OneToMany
+    private List<Job> jobs;
+
+    public Company() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+}
